7742aa17a3e6052873f448946b68f3ee8e64fdf8
AliceLR <petrifiedrowan@gmail.com>
2020-10-01 20:14:57 -0600

    Remove extra formats not required by MegaZeux.

--- src/format.c
+++ src/format.c
@@ -28,28 +28,14 @@
 const struct format_loader *const format_loaders[NUM_FORMATS + 2] = {
 	&libxmp_loader_xm,
 	&libxmp_loader_mod,
-#ifndef LIBXMP_CORE_DISABLE_IT
 	&libxmp_loader_it,
-#endif
 	&libxmp_loader_s3m,
-#ifndef LIBXMP_CORE_PLAYER
 	&libxmp_loader_flt,
 	&libxmp_loader_st,
 	&libxmp_loader_stm,
-	&libxmp_loader_stx,
 	&libxmp_loader_mtm,
 	&libxmp_loader_ice,
-	&libxmp_loader_imf,
-	&libxmp_loader_ptm,
-	&libxmp_loader_mdl,
 	&libxmp_loader_ult,
-	&libxmp_loader_liq,
-	&libxmp_loader_no,
-	&libxmp_loader_masi,
-	&libxmp_loader_masi16,
-	&libxmp_loader_muse,
-	&libxmp_loader_gal5,
-	&libxmp_loader_gal4,
 	&libxmp_loader_amf,
 	&libxmp_loader_asylum,
 	&libxmp_loader_gdm,
@@ -58,44 +44,10 @@ const struct format_loader *const format_loaders[NUM_FORMATS + 2] = {
 	&libxmp_loader_med2,
 	&libxmp_loader_med3,
 	&libxmp_loader_med4,
-	/* &libxmp_loader_dmf, */
-	&libxmp_loader_chip,
-	&libxmp_loader_rtm,
-	&libxmp_loader_pt3,
-	/* &libxmp_loader_tcb, */
-	&libxmp_loader_dt,
-	/* &libxmp_loader_gtk, */
-	/* &libxmp_loader_dtt, */
-	&libxmp_loader_mgt,
-	&libxmp_loader_arch,
-	&libxmp_loader_sym,
-	&libxmp_loader_digi,
-	&libxmp_loader_dbm,
-	&libxmp_loader_emod,
 	&libxmp_loader_okt,
-	&libxmp_loader_sfx,
 	&libxmp_loader_far,
-	&libxmp_loader_umx,
 	&libxmp_loader_hmn,
-	&libxmp_loader_stim,
-	&libxmp_loader_coco,
-	/* &libxmp_loader_mtp, */
-	&libxmp_loader_ims,
 	&libxmp_loader_669,
-	&libxmp_loader_fnk,
-	/* &libxmp_loader_amd, */
-	/* &libxmp_loader_rad, */
-	/* &libxmp_loader_hsc, */
-	&libxmp_loader_mfp,
-	&libxmp_loader_abk,
-	/* &libxmp_loader_alm, */
-	/* &libxmp_loader_polly, */
-	/* &libxmp_loader_stc, */
-	&libxmp_loader_xmf,
-#ifndef LIBXMP_NO_PROWIZARD
-	&libxmp_loader_pw,
-#endif
-#endif /* LIBXMP_CORE_PLAYER */
 	NULL /* list terminator */
 };
 
